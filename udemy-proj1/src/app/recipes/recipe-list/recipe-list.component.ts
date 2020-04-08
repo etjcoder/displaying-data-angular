fd2ce0e0fc50a1ebcf59ee18f9c3f7d5cedd662b
Evan Cleary <evanjcleary@gmail.com>
2020-04-08 18:14:51 -0400

    class update -3

---
+++ udemy-proj1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD')
+  
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
