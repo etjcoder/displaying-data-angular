904c23004f57f51baff926349067dffd0cda3ab5
Evan Cleary <evanjcleary@gmail.com>
2020-04-12 17:02:32 -0400

    udemy update 3 412

--- udemy-proj1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-proj1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,22 +9,13 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD'),
-    new Recipe('A Test Recipe22', 'This is simply a test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD')
-  
-  ]
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
-
-  } 
 
 }
---
+++ udemy-proj1/src/app/recipes/recipe-list/recipe.service.ts
@@ -0,0 +1,49 @@
+import { Recipe } from '../recipe.model'
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service'
+
+@Injectable()
+
+export class RecipeService {
+
+  constructor(
+    private slService: ShoppingListService
+  ) { }
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Schnitzel', 
+      'This is simply a test', 
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+      
+      ),
+    
+    new Recipe('Burger Fat', 
+    'This is simply a test', 
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fphotos%2Ffood-meat-recipe-power-pork-1459693%2F&psig=AOvVaw2T0zhgJ6Q3l1LZfV1-puUs&ust=1586398456222000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICT5Pbg1-gCFQAAAAAdAAAAABAD', 
+    [
+      new Ingredient('Bread', 2),
+      new Ingredient('Patty', 2)
+    ]),
+
+  ]
+
+  getRecipes() {
+      return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients)
+
+  }
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+
+
+}
