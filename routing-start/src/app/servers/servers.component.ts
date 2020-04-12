ea2d95e04b469cb6a1f9e367701803fbc4df5b1b
Evan Cleary <evanjcleary@gmail.com>
2020-04-12 18:07:40 -0400

    udemy 4 412

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
 
+import { Router, ActivatedRoute } from '@angular/router'
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -9,10 +11,19 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private activeRoute: ActivatedRoute ) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    this.router.navigate(['servers'], {relativeTo: this.activeRoute})
+
+
   }
 
 }
