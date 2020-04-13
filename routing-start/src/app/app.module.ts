5eb7030461047c0dc3d844078986a68ea790e7f1
Evan Cleary <evanjcleary@gmail.com>
2020-04-12 23:53:53 -0400

    udemy routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,13 +12,12 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app.routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent }, 
-  { path: 'users', component: UsersComponent  },
-  { path: 'servers', component: ServersComponent },
-  { path: 'users/:id/:name', component: UserComponent }
-]
 
 @NgModule({
   declarations: [
@@ -28,14 +27,15 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 
+import {ActivatedRoute, Router, Params} from '@angular/router'
+
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
@@ -10,10 +12,28 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+
+    this.server = this.serversService.getServer(id);
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      )
+  }
+
+  onEdit() {
+    // this.router.navigate(['/servers', this.server.id, 'edit'])
+    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve'})
+
   }
 
 }
